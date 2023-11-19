e85efc4b16bd120c18dd51f2c3b0600ff0e5f726
Abisoye Ogunmona <113044913+Soye-Coder@users.noreply.github.com>
2023-11-19 08:59:24 +0000

    Fix tests

--- tests/test_flask_app.py
+++ tests/test_flask_app.py
@@ -1,10 +1,16 @@
 import sys
 sys.path.append("./")
 import pytest
+import requests
+
 from unittest.mock import patch
 
 from quote_gen.app import app as gen_app
 from quote_disp.app import app as disp_app
+
+@pytest.fixture
+def app_url():
+    return "http://localhost:5001"
 
 @pytest.fixture
 def gen_client():
@@ -39,9 +45,9 @@ def test_quote_endpoint(mock_randrange, gen_client):
     expected_quote = "The greatest glory in living lies not in never falling, but in rising every time we fall. -Nelson Mandela"
     assert response.data.decode("utf-8") == expected_quote
 
-# def test_quote_disp_endpoint(disp_client):
-#     response = disp_client.get('/get_quote')
-#     assert response.status_code == 200
+def test_quote_disp_endpoint(app_url):
+    response = requests.get(f'{app_url}/get_quote')
+    assert response.status_code == 200
     
 def test_quote_disp_endpoint_health(disp_client):
     response = disp_client.get('/health')
